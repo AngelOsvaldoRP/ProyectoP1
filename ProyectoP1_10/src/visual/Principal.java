5b180ddffe82dce564c3f57da72bc1558ca04d10
WilliamAWSOFTWARE <45134675+WilliamAWSOFTWARE@users.noreply.github.com>
2020-11-23 22:22:25 -0400

    Base de la ventana principal

---
+++ ProyectoP1_10/src/visual/Principal.java
@@ -0,0 +1,225 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+
+import java.awt.Panel;
+
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.border.TitledBorder;
+import javax.swing.JScrollPane;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import javax.swing.border.BevelBorder;
+import java.awt.Color;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Toolkit;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class Principal extends JFrame {
+	private JPanel contentPane;
+	private JTextField fecha_textfield;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal frame = new Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Principal() {
+		setAutoRequestFocus(false);
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage("E:\\Proyectos\\Java\\Programacion 1- WorkSpace\\PrimerProyecto\\src\\assets\\Logo-AC.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1397, 744);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Registro");
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnNewMenu_3 = new JMenu("Equipos");
+		mnNewMenu.add(mnNewMenu_3);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Nuevo Equipo");
+		mnNewMenu_3.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Nuevo Jugador");
+		mnNewMenu_3.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Lesiones");
+		mnNewMenu.add(mntmNewMenuItem_2);
+		
+		JMenu mnNewMenu_1 = new JMenu("Lista");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenu mnNewMenu_2 = new JMenu("Informacion");
+		menuBar.add(mnNewMenu_2);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		Panel panel = new Panel();
+		panel.setBounds(5, 5, 1381, 684);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_position = new JPanel();
+		panel_position.setBorder(new TitledBorder(null, "Tabla de posiciones", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_position.setBounds(1032, 94, 310, 237);
+		panel.add(panel_position);
+		panel_position.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportBorder(null);
+		panel_position.add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JPanel panel_juegosdeldia = new JPanel();
+		panel_juegosdeldia.setBorder(new TitledBorder(null, "Juegos del dia", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_juegosdeldia.setBounds(1032, 358, 310, 278);
+		panel.add(panel_juegosdeldia);
+		panel_juegosdeldia.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setViewportBorder(null);
+		panel_juegosdeldia.add(scrollPane_1);
+		
+		JPanel equipos = new JPanel();
+		equipos.setBorder(new TitledBorder(new CompoundBorder(new LineBorder(new Color(171, 173, 179)), new EmptyBorder(2, 2, 2, 2)), "Equipos", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		equipos.setBounds(10, 454, 919, 182);
+		panel.add(equipos);
+		equipos.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		equipos.add(panel_1);
+		
+		JLabel Logo_aguilas_campeon = new JLabel("");
+		panel_1.add(Logo_aguilas_campeon);
+		Logo_aguilas_campeon.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				JOptionPane.showMessageDialog(null, "El cliente existe", "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		});
+		Logo_aguilas_campeon.setToolTipText("Ver equipo");
+		Logo_aguilas_campeon.setIcon(new ImageIcon("E:\\Proyectos\\Java\\Programacion 1- WorkSpace\\PrimerProyecto\\src\\assets\\Logo-AC.png"));
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		equipos.add(panel_2);
+		
+		JLabel logo_babosos_del_licey = new JLabel("");
+		panel_2.add(logo_babosos_del_licey);
+		logo_babosos_del_licey.setIcon(new ImageIcon("E:\\Proyectos\\Java\\Programacion 1- WorkSpace\\PrimerProyecto\\src\\assets\\Logo-TL.png"));
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		equipos.add(panel_3);
+		
+		JLabel lblNewLabel = new JLabel("");
+		panel_3.add(lblNewLabel);
+		lblNewLabel.setIcon(new ImageIcon("E:\\Proyectos\\Java\\Programacion 1- WorkSpace\\PrimerProyecto\\src\\assets\\Logo-TE.png"));
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		equipos.add(panel_4);
+		
+		JLabel logo_estrellas = new JLabel("");
+		panel_4.add(logo_estrellas);
+		logo_estrellas.setIcon(new ImageIcon("E:\\Proyectos\\Java\\Programacion 1- WorkSpace\\PrimerProyecto\\src\\assets\\Logo-ES.png"));
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		equipos.add(panel_5);
+		
+		JLabel logo_gigantes = new JLabel("");
+		panel_5.add(logo_gigantes);
+		logo_gigantes.setIcon(new ImageIcon("E:\\Proyectos\\Java\\Programacion 1- WorkSpace\\PrimerProyecto\\src\\assets\\Logo-GC.png"));
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		equipos.add(panel_6);
+		
+		JLabel logo_escojido = new JLabel("");
+		panel_6.add(logo_escojido);
+		logo_escojido.setIcon(new ImageIcon("E:\\Proyectos\\Java\\Programacion 1- WorkSpace\\PrimerProyecto\\src\\assets\\Logo-EC.png"));
+		logo_escojido.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		JLabel LogoLidom = new JLabel("");
+		LogoLidom.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			}
+		});
+		LogoLidom.setIcon(new ImageIcon("E:\\Proyectos\\Java\\Programacion 1- WorkSpace\\PrimerProyecto\\src\\assets\\logo lidom.png"));
+		LogoLidom.setBounds(259, 83, 381, 313);
+		panel.add(LogoLidom);
+		
+		JLabel fecha_label = new JLabel("Fecha Actual:");
+		fecha_label.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		fecha_label.setBounds(1152, 66, 102, 14);
+		panel.add(fecha_label);
+		
+	
+		Calendar fecha = new GregorianCalendar();                                 
+	    int year = fecha.get(Calendar.YEAR);
+	    int mes = fecha.get(Calendar.MONTH);
+	    int dia = fecha.get(Calendar.DAY_OF_MONTH);
+	    
+		fecha_textfield = new JTextField();
+		fecha_textfield.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		fecha_textfield.setHorizontalAlignment(SwingConstants.CENTER);
+		fecha_textfield.setEditable(false);
+		fecha_textfield.setBounds(1256, 63, 86, 20);
+		panel.add(fecha_textfield);
+		fecha_textfield.setColumns(10);
+		fecha_textfield.setText(dia+"/"+mes+"/"+year);
+		
+		
+		
+	}
+}
+
+
